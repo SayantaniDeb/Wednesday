85cdf0ebf443d7fa0029af137ee8edd987135d36
Sayantani Deb <sayantani.1001@gmail.com>
2023-01-29 19:03:54 +0530

    initial commit

---
+++ src/components/card.js
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from "react";
+
+export default function Card(){
+    const[apiData,setApiData]=useState([]);
+    function callApi() {
+        fetch('http://www.boredapi.com/api/activity?type', { method: 'GET' })
+          .then(response => response.json())
+          .then(data => setApiData(data) ) // Displays the firstName from the API response
+      }
+      useEffect(()=>{
+        callApi();
+      },[])
+    return(
+        <section className="card-whole">
+        <div className="card">
+            <button onClick={callApi}>Get Advice</button>
+        <div className="content--content">Advice: {apiData.activity}
+            <div className="content--type">Type: {apiData.type}</div>
+        </div>
+        </div>
+        <marquee scrollamount="20"><img src="../images/2.png" className="hand"></img></marquee>
+
+       
+        </section>
+    )
+}
---
+++ src/components/navbar.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Navbar(){
+    return(
+        <>
+            <section className="navbar">
+               
+                <div className="center">
+                    What would Hand Suggest?
+                </div>
+               
+
+            </section>
+        </>
+    )
+}
