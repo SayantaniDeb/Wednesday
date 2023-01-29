d15436e043261d406ae4cad1f1372cbc4f570cac
Sayantani Deb <sayantani.1001@gmail.com>
2023-01-29 19:30:39 +0530

    third commit

--- src/components/card.js
+++ src/components/card.js
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from "react";
 export default function Card(){
     const[apiData,setApiData]=useState([]);
     function callApi() {
-        fetch('http://www.boredapi.com/api/activity?type', { method: 'GET' })
+        fetch('https://www.boredapi.com/api/activity?type', { method: 'GET' })
           .then(response => response.json())
           .then(data => setApiData(data) ) // Displays the firstName from the API response
       }
